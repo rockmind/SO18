c3f946adfc10e861de76615e0e8448f50f0d89da
Błażej Szulc <29uizyd@gmail.com>
2018-10-21 22:06:01 +0200

    Dodanie rozwiacan lab2

---
+++ lab2/zad2/main.c
@@ -0,0 +1,190 @@
+#define _GNU_SOURCE
+#define _XOPEN_SOURCE
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<dirent.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
+#define ERR_N_ARGS   "Błędna liczba argumentów programu"
+#define ERR_2_ARGS   "Błędna drugi argument. Możliwe: '>', '<', '='"
+#define ERR_3_ARGS   "Błędna trzeci argument. Zły format lub błędna data. Możliwy format: yyyy-MM-dd"
+#define ERR_3_ARGS_Y "Błędna trzeci argument. Błędny rok"
+#define ERR_3_ARGS_D "Błędna trzeci argument. Błędny dzień"
+#define ERR_3_ARGS_M "Błędna trzeci argument. Błędny miesiąc"
+#define ERR_dir      "Nie można otworzyć katalogu"
+
+void validate_arguments(int argc, char **argv){
+	if(argc!=4){
+		fprintf(stderr,"%s\n",ERR_N_ARGS);
+		exit(EXIT_FAILURE);
+	}
+	if(strcmp(argv[2], "=") != 0 && strcmp(argv[2], ">") != 0 && strcmp(argv[2], "<") != 0){
+		fprintf(stderr,"%s\n",ERR_2_ARGS);
+		exit(EXIT_FAILURE);
+	}
+	if(strlen(argv[3])   !=  10
+			||argv[3][0] <  48 
+			||argv[3][0] >  57 
+			||argv[3][1] <  48 
+			||argv[3][1] >  57 
+			||argv[3][2] <  48 
+			||argv[3][2] >  57 
+			||argv[3][3] <  48 
+			||argv[3][3] >  57 
+			||argv[3][4] != '-'
+			||argv[3][5] <  48 
+			||argv[3][5] >  57 
+			||argv[3][6] <  48 
+			||argv[3][6] >  57 
+			||argv[3][7] != '-'
+			||argv[3][8] <  48 
+			||argv[3][8] >  57 
+			||argv[3][9] <  48 
+			||argv[3][9] >  57
+	){
+		fprintf(stderr,"%s\n",ERR_3_ARGS);
+		exit(EXIT_FAILURE);
+	}
+	int yy = 1000*(argv[3][0]-48) + (100*argv[3][1]-48) + (10*argv[3][2]-48) + (argv[3][3]-48);
+	int mm = 10*(argv[3][5]-48) + (argv[3][6]-48);
+	int dd = 10*(argv[3][8]-48) + (argv[3][9]-48);
+	if(yy>=1900 && yy<=9999){
+		if(mm>=1 && mm<=12)
+		{
+			//check days
+			if( !(
+					((dd>=1 && dd<=31) && (mm==1 || mm==3 || mm==5 || mm==7 || mm==8 || mm==10 || mm==12))
+				  ||((dd>=1 && dd<=30) && (mm==4 || mm==6 || mm==9 || mm==11))
+				  ||((dd>=1 && dd<=28) && (mm==2))
+				  ||(dd==29 && mm==2 && (yy%400==0 ||(yy%4==0 && yy%100!=0)))
+				)
+			){
+				fprintf(stderr,"%s: %s: %d\n",argv[3],ERR_3_ARGS_D,dd);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else {
+			fprintf(stderr,"%s: %s: %d\n",argv[3],ERR_3_ARGS_M,mm);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else {
+		fprintf(stderr,"%s: %s: %d\n",argv[3],ERR_3_ARGS_Y,yy);
+		exit(EXIT_FAILURE);
+	}
+}
+void validate_dir(char* dirname, DIR* dir){
+	if(!dir){
+		fprintf(stderr,"%s: %s\n",dirname,ERR_dir);
+		exit(EXIT_FAILURE);
+	}
+}
+void czytaj(char* dirname,DIR* katalog,int data_put, char eq){
+	struct stat path_stat;
+	struct dirent* plik=readdir(katalog);
+	char* nazwa_pliku;
+	chdir(dirname);
+	while(plik){
+		nazwa_pliku=plik->d_name;
+		lstat(nazwa_pliku, &path_stat);
+		if(strcmp(nazwa_pliku, ".") != 0 && strcmp(nazwa_pliku, "..") != 0 && !S_ISLNK(path_stat.st_mode)){
+			if(S_ISDIR(path_stat.st_mode)){
+				DIR* katalog2 = opendir(nazwa_pliku);
+				validate_dir(nazwa_pliku,katalog2);
+				
+				czytaj(nazwa_pliku,katalog2,data_put,eq);
+				closedir(katalog2);
+			}
+			else{
+				struct tm* tm_file;
+				tm_file=gmtime(&path_stat.st_mtime);
+				int data_file=10000*(tm_file->tm_year + 1900) + 100*(tm_file->tm_mon) + (tm_file->tm_mday);
+				int flaga=0;
+				switch(eq){
+					case '=':
+						if(data_file==data_put){
+							flaga=1;
+						}
+						break;
+					case '>':
+						if(data_file>data_put){
+							flaga=1;
+						}
+						break;
+					case '<':
+						if(data_file<data_put){
+							flaga=1;
+						}
+						break;
+				}
+				if(flaga){
+					printf("%s/%s \t%jd\t -",get_current_dir_name(),nazwa_pliku,path_stat.st_size);
+					printf( (path_stat.st_mode & S_IRUSR) ? "r" : "-");
+					printf( (path_stat.st_mode & S_IWUSR) ? "w" : "-");
+					printf( (path_stat.st_mode & S_IXUSR) ? "x" : "-");
+					printf( (path_stat.st_mode & S_IRGRP) ? "r" : "-");
+					printf( (path_stat.st_mode & S_IWGRP) ? "w" : "-");
+					printf( (path_stat.st_mode & S_IXGRP) ? "x" : "-");
+					printf( (path_stat.st_mode & S_IROTH) ? "r" : "-");
+					printf( (path_stat.st_mode & S_IWOTH) ? "w" : "-");
+					printf( (path_stat.st_mode & S_IXOTH) ? "x" : "-");
+					printf("\t%d-%02d-%02d\n",1900+tm_file->tm_year,1+tm_file->tm_mon,tm_file->tm_mday);
+				}
+			}
+		}
+		plik=readdir(katalog);
+	}
+	chdir("..");
+}
+int main(int argc, char **argv){
+	validate_arguments(argc, argv);
+	const char* date_arg = argv[3];
+	struct tm tm_arg;
+	strptime(date_arg, "%Y-%m-%d", &tm_arg);
+	int data_puts=10000*(tm_arg.tm_year + 1900) + 100*(tm_arg.tm_mon) + (tm_arg.tm_mday);
+	char* dirname = argv[1];
+	DIR* katalog = opendir(dirname);
+	validate_dir(dirname,katalog);
+	czytaj(dirname,katalog,data_puts,argv[2][0]);
+	//struct stat path_stat;
+	//struct dirent* plik=readdir(katalog);
+	//while(plik){
+	//	stat(plik->d_name, &path_stat);
+	//	//	S_ISREG(path_stat.st_mode);
+	//	printf("%s dir? %d\n",plik->d_name,!S_ISREG(path_stat.st_mode));
+	//	plik=readdir(katalog);
+	//}
+    //chdir("all");
+    //chdir("..");
+    //printf("Current working dir: %s\n", get_current_dir_name());
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	closedir(katalog);
+	exit(EXIT_SUCCESS);
+}
